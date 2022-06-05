2f16b83b4810de9c7d6e87b7339d9db490f75f32
Kahana <Microsoft@Wegerson.com>
2022-06-05 09:16:22 -0600

    GetTableNames Extension Files

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,12 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Json.Orm;
+using Json.Orm.Extensions;
+using System.Data.SqlClient;
 using TestConsole;
 
-Console.WriteLine("Hello, World!");
+
 
 var connectionStr = @"Data Source=.;Integrated Security=SSPI;Initial Catalog=Rasa";
 
-//var jdb = new JsonOrmDatabase(connectionStr);
+var jdb = new JsonOrmDatabase(connectionStr);
 
 //string raw = jdb.GetRawSQL("[get].[Project]");
 
@@ -20,12 +22,31 @@ var connectionStr = @"Data Source=.;Integrated Security=SSPI;Initial Catalog=Ras
 //Console.WriteLine(one.Tpc);
 //cities.Dump();
 
+// Standard Fluent
+//var result = await JsonOrmDatabase.Create(connectionStr)
+//                                  .SetStoredProcedure("[get].[PicklistMenus]")
+//                                  .GetJsonAsync<List<DetailCategory>>();
 
-var result = await JsonOrmDatabase.Create(connectionStr)
-                                  .SetStoredProcedure("[get].[PicklistMenus]")
-                                  .GetJsonAsync<List<DetailCategory>>();
+
+
+//Console.WriteLine(result[0].Name);
+
+// Raw SQL call
 
 
 
-Console.WriteLine(result[0].Name);
+//var names = new SqlConnection(connectionStr).GetTableNames("info")
+//                                            .Select(nm => $"select top(1) * from info.{nm} for json auto;")
+//                                            .ToList();
+
+//Console.WriteLine(string.Join(Environment.NewLine, names));
+
+//var json = jdb.GetRawSQLByDynamicSql(names[10]);
+
+//Console.WriteLine(json);
+
+var TableJsons = jdb.GetJsonForTables("info");
+
+Console.WriteLine(TableJsons.Any() ? "True" : "False");
+
 
---
+++ Vector/Extensions/SqlExtensions.cs
@@ -0,0 +1,60 @@
+﻿using Json.Orm.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json.Orm.Extensions
+{
+    public static class SQLExtensions
+    {
+        public static List<string?> GetTableNames(this SqlConnection connection, string tableSchema = "dbo")
+        {
+            using (connection)
+            {
+                connection.Open();
+
+                return connection.GetSchema("Tables").Rows
+                                 .Cast<DataRow>()
+                                 .Where(row => row["TABLE_SCHEMA"].Equals(tableSchema))
+                                 .Select(row => row["TABLE_NAME"].ToString())
+                                 .OrderBy(nme => nme)
+                                 .ToList();
+
+            } // Auto Close exiting the block
+        }
+
+        public static List<TableJsonMeta> GetJsonForTables(this JsonOrmDatabase db,
+                                                     string tableSchema = "dbo",
+                                                     Func<string, bool> tableFilter = null)
+        {
+            
+
+            var tables = db.Connection?.GetTableNames(tableSchema)
+                                       .Where(nm => tableFilter?.Invoke(nm) ?? true)
+                                       .Select(nm => new TableJsonMeta(nm, tableSchema))
+                                       .ToList();
+
+            if (!tables?.Any() ?? true)
+                return new List<TableJsonMeta>();
+            
+            // Reset to a new connection.
+            db.Connection = null;
+
+            using (db.Connection)
+            {
+                db.Connection.Open();
+
+                tables.ForEach(tbl => tbl.Json = db.GetRawSQLByDynamicSql(tbl.Sql));
+            }
+
+            return tables;
+
+        }
+
+
+    }
+}
